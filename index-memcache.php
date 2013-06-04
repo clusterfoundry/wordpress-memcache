ea64225c00ac5ad1882d57b629d77bf4a31acc48
Jayson Cena <jayson.cena@gmail.com>
2013-06-04 23:50:54 +0800

    Add credits and GPLv2 license notice

--- index-memcache.php
+++ index-memcache.php
@@ -1,4 +1,29 @@
 <?php
+// Copyright (C) 2013 Jayson Cena, ClusterFoundry (jayson.cena at clusterfoundry.com)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//
+// By: Jayson Cena
+// Based from http://eamann.com/tech/ludicrous-speed-wordpress-caching-with-redis/ but using memcached instead of redis
+// Serves cache in 0.5ms/0.0005 seconds from a 64GBRAM/Intel Xeon 6-Core,12 threads/2x2TB SATA3
+// Running on NginX and PHP5-FPM
+//
+// Can also be used on other PHP apps aside from Wordpress with few modifications
+//
+
 $debug = 1;
 $start = microtime();
 
